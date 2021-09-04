8d4c65229569975978f53495b8e6d39b4f29061a
Seth Bowman <brandonsethbowman@gmail.com>
2021-09-04 10:55:57 -0500

    Completed component, next will be styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,24 @@
 import classes from './About.module.css';
 import PageHeader from './../PageHeader/PageHeader.js';
+import me from './../../images/headshot.jpg'
 
 const About = () => {
     return ( 
-        <div>
-            <PageHeader title={'About Me'} />            
+        <div className={classes.AboutMe}>
+            <PageHeader title={'About Me'} />
+            <div className={classes.Container}>
+                <div className={classes.Text}>
+                    <h2>Seth Bowman</h2>
+                    <p>
+                        I am a certified web developer/software engineer working as a teaching assistant for a company called 'TrueCoders' based out of Hoover, AL.
+                        Prior to this role, I completed two boot camps with TrueCoders, where I honed my programming skills. Ideally, I would like to continue to specialize in web development.                        
+                    </p>
+                    <div className={classes.Photo}>
+                    <img className={classes.Me} src={me} alt="Seth" />
+                    </div>
+                </div>
+
+            </div>            
         </div>
      );
 }
