53370e27ba9343eb99db89165bad55df0c882dcc
Seth Bowman <brandonsethbowman@gmail.com>
2021-09-05 17:47:50 -0500

    Added icons with links to personal pages

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,17 @@
+import { FaGithub, FaFacebookSquare, FaLinkedin, FaPlaystation } from "react-icons/fa";
+
+const handleURL = (url) => {
+    return () => window.open(url, "_blank")
+}
+
 const Contact = () => {
     return ( 
         <div>
-            <h1>Contact</h1>
+            <FaGithub color='#0094c8' size='30px' style={{padding: '1%'}} onClick={handleURL('https://github.com/SethBowman')} />
+            <FaFacebookSquare color='#0094c8' size='30px' style={{padding: '1%'}} onClick={handleURL('https://www.facebook.com/OGsethbowman/')} />
+            <FaLinkedin color='#0094c8' size='30px' style={{padding: '1%'}} onClick={handleURL('https://www.linkedin.com/in/seth-bowman-358863211/')} />
+            <FaPlaystation color='#0094c8' size='30px' style={{padding: '1%'}} onClick={handleURL('https://psnprofiles.com/Captn-Kold')} />
+            
         </div>
      );
 }
