93102de97b9f60df72344f09daa5079b40086709
Seth Bowman <brandonsethbowman@gmail.com>
2021-09-04 23:46:14 -0500

    Created 3 variables with ul containing skills, created an array to hold them

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,10 +1,31 @@
 import classes from "./Skills.module.css";
 import PageHeader from "./../PageHeader/PageHeader.js";
 
+const backEndSkills = 
+<ul>
+    <li>BACKEND</li>
+    <li>C# - SQL - ASP.Net MVC</li>
+</ul>
+
+const frontEndSkills =
+<ul>
+    <li>FRONTEND</li>
+    <li>HTML - CSS = JavaScript</li>
+    <li>React.js = Node.js - Express.js</li>
+</ul>
+
+const otherSkills =
+<ul>
+    <li>OTHER</li>
+    <li>Git - Json - XML</li>
+</ul>
+
+const allSkills = [backEndSkills, frontEndSkills, otherSkills];
+
 const Skills = () => {
   return (
     <div>
-      <PageHeader title={"Languages & Skills"} />
+      <PageHeader title={"Check out my skills!"} />
       <p>
         I earned certification in web development and software engineering
         through TrueCoders and current work as an instructor there. During my
@@ -12,6 +33,15 @@ const Skills = () => {
         Below are some programming languages, frameworks, toolkits, and
         libraries that I have worked with and liked.
       </p>
+      <div className={classes.Container}>
+            {allSkills.map(skills => {
+                return (
+                    <div className={classes.List}>
+                        {skills}
+                    </div>
+                )
+            })}
+      </div>
     </div>
   );
 };
