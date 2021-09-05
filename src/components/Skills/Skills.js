95fec40e603dd1cec6ca13557e732dd8ba7e68f0
Seth Bowman <brandonsethbowman@gmail.com>
2021-09-04 23:20:42 -0500

    Imported css and PageHeader component, added intro paragraph

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,9 +1,19 @@
+import classes from "./Skills.module.css";
+import PageHeader from "./../PageHeader/PageHeader.js";
+
 const Skills = () => {
-    return ( 
-        <div>
-            <h1>Skills</h1>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <PageHeader title={"Languages & Skills"} />
+      <p>
+        I earned certification in web development and software engineering
+        through TrueCoders and current work as an instructor there. During my
+        time through the course, and even still, I've created many projects.
+        Below are some programming languages, frameworks, toolkits, and
+        libraries that I have worked with and liked.
+      </p>
+    </div>
+  );
+};
+
 export default Skills;
